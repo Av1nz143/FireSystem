d7aacd26c256a5d6813e0bc321669bb7131ca483
Delli <poudel.delli@gmail.com>
2018-02-13 19:14:27 -0800

    Transmitter T validations
    Updating validation for Transmitter T records

--- app/models/transmitter_t_record.rb
+++ app/models/transmitter_t_record.rb
@@ -42,7 +42,148 @@ class TransmitterTRecord < ApplicationRecord
     require 'csv'
 
     def self.validations
-      {}
+      {
+        record_type: {
+          required: "T",
+          length: 1
+        },
+        payment_year: {
+          required: "2017"
+          length: 4
+        },
+        prior_year_data_indicator: {
+          required: "P",
+          length: 1
+        },
+        transmitters_tin: {
+          required: nil,
+          length: 9
+        },
+        tranmitter_control_code: {
+          required: nil,
+          length: 5
+        },
+        blank: {
+          required: nil,
+          length: 7
+        },
+        test_file_indicator: {
+          required: nil,
+          length: 1
+        },
+        foreign_entity_indicator: {
+          required: nil,
+          length: 1
+        },
+        transmitter_name: {
+          required: nil,
+          length: 80
+        },
+        company_name: {
+          required: nil,
+          length: 80
+        },
+        company_name: {
+          required: nil,
+          length: 80
+        },
+        company_mailing_address: {
+          required: nil,
+          length: 40
+        },
+        comapny_city: {
+          required: nil,
+          length: 40
+        },
+        company_state: {
+          required: nil,
+          length: 2
+        },
+        company_zip_code: {
+          required: nil,
+          length: 9
+        },
+        blank: {
+          required: nil,
+          length: 15
+        },
+        total_no_of_payees: {
+          required: nil, 
+          length: 8
+        },
+        contact_name: {
+          required: nil,
+          length: 40
+        },
+        contact_telephone_number_and_etension: {
+          required: nil,
+          length: 15
+        },
+        contact_email_address: {
+          required: nil,
+          length: 50
+        },
+        blank: {
+          required: nil,
+          length: 91
+        },
+        record_sequence_number: {
+          required: nil,
+          length: 8
+        },
+        blank: {
+          required: nil,
+          length: 10
+        },
+        vendor_indicator: {
+          required: nil,
+          length: 1
+        },
+        vendor_name: {
+          required: nil,
+          length:40
+        },
+        vendor_mailing_address: {
+          required: nil,
+          length: 40
+        },
+        vendor_city: {
+          required: nil,
+          length: 40
+        },
+        vendor_state: {
+          required: nil,
+          length: 2
+        },
+        vendor_zip_code: {
+          required: nil,
+          length: 9
+        },
+        vendor_contact_name: {
+          required: nil,
+          length: 40
+        },
+        vendor_contact_telephone_number_extension: {
+          required: nil,
+          length: 15
+        },
+        blank: {
+          required: nil,
+          length: 35
+        },
+        vendor_foreign_entity_indicator: {
+          required: nil,
+          length: 1
+        },
+        blank: {
+          required: nil,
+          length: 8
+        },
+        blank: {
+          required: nil,
+          length: 2
+        },
+      }
     end
 
     def self.to_csv
